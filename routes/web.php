6db942c46fc7bfc8fb1dbad074efd8aacc14155b
pepesan <pepesan@gmail.com>
2020-07-28 02:57:34 +0200

    ejemplos de usos de rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,62 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+ * Rutas por método
+ */
+Route::get('rutas/', function () {
+    return '/rutas vía get';
+});
+
+Route::post('rutas/', function () {
+    return '/rutas vía post';
+});
+
+Route::put('rutas/', function () {
+    return '/rutas vía put';
+});
+
+Route::delete('rutas/', function () {
+    return '/rutas vía delete';
+});
+
+/*
+ * Paso de parámetros
+ */
+Route::get('params/{id}', function ($id) {
+    return 'Param '.$id;
+});
+// Varios parámetros
+Route::get('params/{mes}/{dia}', function ($mes, $dia) {
+    return 'Param mes:'.$mes."- dia:". $dia;
+});
+// Parámetros Opcionales
+Route::get('params-opc/{mes?}', function ($mes="ns") {
+    return 'Param mes:'.$mes;
+});
+// Constraints
+Route::get('valida/{name}', function ($name) {
+    return 'Param name:'.$name;
+})->where('name', '[A-Za-z]+');
+// Rutas nombradas
+Route::get('named', function () {
+    return "Named Route";
+})->name('named');
+// Redirecciones
+Route::get('redir-named', function () {
+    // calculo de ruta
+    $url = route('named');
+    // redirección a ruta
+    return redirect()->route('named');
+});
+// Redirección por URL
+Route::redirect('/home', '/rutas');
+// Prefijos
+Route::prefix('prefijo')->group(function () {
+    Route::get('ruta', function () {
+        // /prefijo/ruta
+        return "prefijo/ruta";
+    });
+});
+
