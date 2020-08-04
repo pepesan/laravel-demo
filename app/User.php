9352f658d82924cc027738570b8c01c849ab201d
pepesan <pepesan@gmail.com>
2020-08-04 17:35:38 +0200

    commit de mierda

--- app/User.php
+++ app/User.php
@@ -44,4 +44,12 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Phone');
     }
+
+    public function generateToken()
+    {
+        $this->api_token = str_random(60);
+        $this->save();
+
+        return $this->api_token;
+    }
 }
